955228276d2795133ec3c33efebb948f73a598e2
samashourbagyy <samashourbagy12@gmail.com>
2025-12-13 03:21:22 +0200

    main added

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int calculate(int a, int b) {
+    return a + b;   // original version
+}
+
+int main() {
+    cout << "Result: " << calculate(5, 3) << endl;
+    return 0;
+}
